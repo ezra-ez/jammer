a4e22eb35e924c68aa839e0cfad92bc5aa4e15ee
ezra-ez <prasetyoezra1@gmail.com>
2024-12-05 19:09:12 +0700

    jammer_main.ino - kept for testing

--- jammer_main.ino
+++ jammer_main.ino
@@ -1,30 +1,24 @@
-// source: https://github.com/WOD-MN/bt_jam/blob/main/BLE_JAMMER_HSPI_PIN.ino
+
 // NRF24 Connection
 // SCK=14, MISO=12, MOSI=13, CS=15, CE=16
 
 
 #include "RF24.h"
-#include <ezButton.h>
 #include "esp_bt.h"
-#include "esp_wifi.h"
 
 SPIClass *hp = nullptr;
 
 RF24 radio(16, 15, 16000000);
 
-byte i = 45;
-
-ezButton toggleSwitch(33);
+byte i = 2;
 
 
 void setup(void) {
   esp_bt_controller_deinit();
-  esp_wifi_stop();
-  esp_wifi_deinit();
   Serial.begin(115200);
-  toggleSwitch.setDebounceTime(50);
   initHP();
 }
+
 void initHP() {
   hp = new SPIClass(HSPI);
   hp->begin();
@@ -47,6 +41,8 @@ void initHP() {
 }
 
 void one() {
+
+
   ////SWEEP CHANNEL
   for (int i = 2; i <= 94; i = i+5) {
     radio.setChannel(i);
@@ -57,17 +53,5 @@ void one() {
 }
 
 void loop(void) {
-
-  toggleSwitch.loop();  // MUST call the loop() function first
-
-  /* if (toggleSwitch.isPressed())
-    Serial.println("one");
-  if (toggleSwitch.isReleased())
-    Serial.println("two");*/
-
-  int state = toggleSwitch.getState();
-
-  if (state == LOW)
-    one();
-
+  one();
 }
